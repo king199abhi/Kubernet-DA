f1d3c7926b595c704ec4a0bbf5155392783272b9
king199abhi <abistar777@gmail.com>
2025-04-15 00:24:44 +0530

    test for app.py

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return '🎉 Hello from your Minikube-powered Flask app!'
+
+@app.route('/health')
+def health():
+    return '✅ Healthy', 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
